e5d18a024c75daf5039e8cd07da8e1e12764810d
Roman Guivan <Roman.Guivan@mercedes-benz.io>
2019-01-11 12:47:53 +0100

    NO MORE CELLS

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { interval } from 'rxjs';
 import Graphics from './components/Graphics';
 import Controls from './components/Controls';
 
-const WIDTH = 800, HEIGHT = 600, CELL_SIZE = 40;
+const WIDTH = 800, HEIGHT = 600, CELL_SIZE = 40, VELOCITY = 20;
 const max_X = Math.floor(WIDTH/CELL_SIZE), 
       max_Y =  Math.floor(HEIGHT/CELL_SIZE), 
-      startPoint = {x: 7, y: 7},
+      startPoint = {x: 7* CELL_SIZE, y: 7*CELL_SIZE},
       pos = {...startPoint},
-      TICK = 80;
+      RENDER_TICK = 40;
 const app = new PIXI.Application(800, 600, {backgroundColor : 0x00});
 const g = new Graphics();
 
@@ -27,41 +27,53 @@ Controls.$events.subscribe((direction)=>{
   currentDirection = direction;
 });
   
-const source = interval(TICK);
+const source = interval(RENDER_TICK);
+
 const subscribe = source.subscribe(val => render(val));
+
 g.drawGrid(WIDTH, HEIGHT, CELL_SIZE);
   
 function render(step) {
   g.clear();
   g.drawGrid(WIDTH, HEIGHT, CELL_SIZE);
+  renderPickups();
+  collisionCheck();
   const isMoving = !!currentDirection;
   if (isMoving) {
     updatePos();
-    g.cell(pos.x, pos.y, CELL_SIZE);
+    g.rect(pos.x, pos.y, CELL_SIZE, CELL_SIZE);
   } else {
     // stay in place
-    g.cell(startPoint.x, startPoint.y, CELL_SIZE);
+    g.rect(startPoint.y, startPoint.y , CELL_SIZE, CELL_SIZE);
   }
+}
+
+function renderPickups() {
+
+}
+
+function collisionCheck() {
+
 }
 
 function updatePos() {
   switch (currentDirection) {
-    case 'up' : pos.y-=1; break;
-    case 'down' : pos.y +=1; break;
-    case 'left': pos.x -=1; break;
-    case 'right': pos.x +=1; break;
+    case 'up' : pos.y-=VELOCITY; break;
+    case 'down' : pos.y +=VELOCITY; break;
+    case 'left': pos.x -=VELOCITY; break;
+    case 'right': pos.x +=VELOCITY; break;
   }
   // pacman-like going out of bounds
-  if (pos.x > max_X - 1) {
+  if (pos.x > WIDTH - 1) {
     pos.x = 0;
   }
   if (pos.x < 0 ) {
-    pos.x = max_X - 1;
+    pos.x = WIDTH - 1;
   }
-  if (pos.y > max_Y - 1){
+  if (pos.y > HEIGHT - 1){
     pos.y = 0;
   }
   if (pos.y < 0) {
-    pos.y = max_Y - 1;
+    pos.y = HEIGHT - 1;
   }
 }
